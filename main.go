fd4017c188fedeb211729da5b05c825289942dbe
infenj2a <blueinfenj2a@gmail.com>
2020-08-26 21:26:17 +0900

    21:26

--- main.go
+++ main.go
@@ -3,17 +3,16 @@
 package main
 
 import (
-	"io"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
-}
-
 func main() {
 	port := os.Getenv("PORT")
-	http.HandleFunc("/", hello)
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!!")
+	})
 	http.ListenAndServe(":"+port, nil)
 }
