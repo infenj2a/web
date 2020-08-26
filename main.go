d46cefd8d5d8beaef3155b4ff0f58973499e5d9b
infenj2a <blueinfenj2a@gmail.com>
2020-08-26 20:54:08 +0900

    20:54

---
+++ main.go
@@ -0,0 +1,19 @@
+// main.go
+
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World!")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":"+port, nil)
+}
