f886f5d1a0655d8edefb7bc15cabdaf938741bb7
infenj2a <blueinfenj2a@gmail.com>
2020-08-27 13:12:57 +0900

    13:12

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	router.LoadHTMLGlob("view/*.html")
 
 	router.GET("/", controller.HelloPage)
-	router.POST("/", controller.PostPage)
+	router.GET("/get", controller.PostPage)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
