4b96b45d4c4e78611a7bf0e27c117cd92cd2db72
infenj2a <blueinfenj2a@gmail.com>
2020-08-27 20:26:13 +0900

    20:26

--- util/db.go
+++ util/db.go
@@ -4,16 +4,14 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
-	// "os"
-	"fmt"
+	"os"
 )
 
 func InitDB() *sqlx.DB {
-	db, err := sqlx.Open("postgres", "host=ec2-54-91-178-234.compute-1.amazonaws.com user=zsendmswafsfvc dbname=d304v44t1lic6h sslmode=require password=f8286a9a23f6af42df5aa2ff7d80e3432f8e073c3ccb454c1ada162d5e45831a")
+	// db, err := sqlx.Open("postgres", "host=ec2-54-91-178-234.compute-1.amazonaws.com user=zsendmswafsfvc dbname=d304v44t1lic6h sslmode=require password=f8286a9a23f6af42df5aa2ff7d80e3432f8e073c3ccb454c1ada162d5e45831a")
+	db, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
-		fmt.Println("NG")
 		log.Fatal(err)
 	}
-	fmt.Println("OK")
 	return db
 }
