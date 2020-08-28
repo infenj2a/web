b468e90cfa0b5ae8fd490fccf8a2dae0fcc3ed7d
infenj2a <blueinfenj2a@gmail.com>
2020-08-28 20:11:45 +0900

    20:11

--- view/css_chat/js/index.js
+++ view/css_chat/js/index.js
@@ -69,3 +69,18 @@ function escape_html (string) {
       }[match]
     });
 }
+
+function kakunin(btnNo){
+  if (btnNo == 1){
+    link = "Yahoo!Japan";
+    href = "http://www.yahoo.co.jp/";
+  }else{
+    link = "Google";
+    href = "http://www.google.co.jp/";
+  }
+
+  ret = confirm(link + "へ飛びます。宜しいですか？");
+  if (ret == true){
+    location.href = href;
+  }
+}
