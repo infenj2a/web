bb99095ececba90c50eb91bc385a4e50f4bde107
infenj2a <blueinfenj2a@gmail.com>
2020-08-28 14:17:08 +0900

    14:17

--- view/css_chat/js/index.js
+++ view/css_chat/js/index.js
@@ -1,4 +1,4 @@
-const url = "https://" + window.location.host + window.location.pathname + "/ws";
+const url = "ws://" + window.location.host + window.location.pathname + "/ws";
 const ws = new WebSocket(url);
 const name = localStorage.getItem("user")
 const chat = document.getElementById("chat");
