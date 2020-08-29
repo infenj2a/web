45d9af43af7aff08c35bc58f8b704ce5223fa9bd
infenj2a <blueinfenj2a@gmail.com>
2020-08-29 15:57:38 +0900

    Portfolio

--- util/db.go
+++ util/db.go
@@ -7,14 +7,18 @@ import (
 )
 
 func InitDB() *sqlx.DB {
-	// 接続情報は heroku config :get pacific-bayou-32131 で取得
-	db, err := sqlx.Open("postgres", "host=ec2-54-91-178-234.compute-1.amazonaws.com user=zsendmswafsfvc dbname=d304v44t1lic6h sslmode=require password=f8286a9a23f6af42df5aa2ff7d80e3432f8e073c3ccb454c1ada162d5e45831a")
+	// 接続情報は heroku config :get リポジトリの名前 で取得可能
+	// デフォルトsslmodeはdisable
+	db, err := sqlx.Open("postgres", "host= user= dbname= sslmode=require password=")
+	// 上記の記載が面倒な場合は下記で実行が可能
 	// db, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
-	//defer db.Close()を付与するとInitDBが終わった際に閉じてしまうので付与していない
+
+	// defer db.Close()を付与するとInitDBが終わった際に閉じてしまうので付与していない
+	// 接続数に限りがあるので将来性を考えるなら考慮するべきところ
 	if err != nil {
 		log.Fatal(err)
 	}
-	//articlesテーブルが無ければ作成
+	// articlesテーブルが無ければ作成
 	query := `CREATE TABLE IF NOT EXISTS articles(
 		id SERIAL NOT NULL,
 		title VARCHAR(40),
