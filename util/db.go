9d34fc1bab7f328b3631b2ae384943f47ccffd67
infenj2a <blueinfenj2a@gmail.com>
2020-09-05 22:55:08 +0900

    Session Add

--- util/db.go
+++ util/db.go
@@ -4,14 +4,13 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
 
 func InitDB() *sqlx.DB {
 	// 接続情報は heroku config :get リポジトリの名前 で取得可能
 	// デフォルトsslmodeはdisable
-	db, err := sqlx.Open("postgres", "host=ec2-54-91-178-234.compute-1.amazonaws.com user=zsendmswafsfvc dbname=d304v44t1lic6h sslmode=require password=f8286a9a23f6af42df5aa2ff7d80e3432f8e073c3ccb454c1ada162d5e45831a")
-	// 上記の記載が面倒な場合は下記で実行が可能
-	// db, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
+	db, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	// defer db.Close()を付与するとInitDBが終わった際に閉じてしまうので付与していない
 	// 接続数に限りがあるので将来性を考えるなら考慮するべきところ
